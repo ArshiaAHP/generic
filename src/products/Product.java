a7fc0480f2bc716fd70a3c557e07ac13641bf112
ArshiaAHP <diniodduck@gmail.com>
2025-04-26 20:55:27 +0330

    add product to products package.

---
+++ src/products/Product.java
@@ -0,0 +1,44 @@
+package products;
+
+public abstract class Product {
+    private String title;
+    private double price;
+    private String id;
+
+    public Product(String title,double price){
+        this.title = title;
+        this.price = price;
+    }
+
+    public String toString(){
+        return title + " | " + price + " | " + id;
+    }
+
+    protected abstract String generateID();
+
+    //setter & getter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
